6d0b4b2c1f6e082f223dc74b54ce9bd0955c40d4
sarvaniYellajosyula <sarvani.yellajosyula@accenture.com>
2020-10-08 13:57:41 +0530

    Update WCHelloWorld3.js

--- WCHelloWorld3.js
+++ WCHelloWorld3.js
@@ -1,4 +1,4 @@
-(function() {
+(function()  {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
     `;
@@ -6,54 +6,55 @@
     customElements.define('com-sac-customwidget-helloworld3', class HelloWorld3 extends HTMLElement {
 
 
-        constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({mode: "open"});
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-			this._tagContainer;
+            this._tagContainer;
             this._tagType = "h1";
             this._tagText = "Hello World";
-			
-        }
+
+		}
 
         //Fired when the widget is added to the html DOM of the page
-        connectedCallback() {
+        connectedCallback(){
             this._firstConnection = true;
-            this.redraw();
+            this.redraw();       
         }
 
-        //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback() {
-
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
         }
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-        onCustomWidgetBeforeUpdate(oChangedProperties) {
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
 
-        }
+		}
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-        onCustomWidgetAfterUpdate(oChangedProperties) {
-            if (this._firstConnection) {
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
                 this.redraw();
             }
         }
-
+        
         //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy() {}
+        onCustomWidgetDestroy(){
+        
+        }
 
-
+        
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // Commented out by default
         /*
         onCustomWidgetResize(width, height){
-            redraw()
+        
         }
         */
 
-		//Getters and Setters
+        //Getters and Setters
         get widgetText() {
             return this._tagType;
         }
@@ -62,9 +63,9 @@
             this._tagText = value;
         }
         // End - Getters and Setters
-		
-        redraw() {
-			if (this._tagContainer){
+
+        redraw(){
+            if (this._tagContainer){
                 this._tagContainer.parentNode.removeChild(this._tagContainer);
             }
 
@@ -73,6 +74,9 @@
             var theText = document.createTextNode(this._tagText);    
             this._tagContainer.appendChild(theText); 
             this._shadowRoot.appendChild(this._tagContainer);
-		}
+        }
+    
+    
     });
+        
 })();
