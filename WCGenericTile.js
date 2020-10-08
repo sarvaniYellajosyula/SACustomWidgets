8d3ed5784b63e463e2aae37681c2b8688871aaf7
sarvaniYellajosyula <sarvani.yellajosyula@accenture.com>
2020-10-08 19:51:35 +0530

    Update WCGenericTile.js

--- WCGenericTile.js
+++ WCGenericTile.js
@@ -35,127 +35,38 @@
      class GenericTile extends HTMLElement {
          constructor() {
              super(); 
-             _shadowRoot = this.attachShadow({
-                 mode: "open"
-             });
+             _shadowRoot = this.attachShadow({ mode: "open" });
              _shadowRoot.appendChild(tmpl.content.cloneNode(true));
-
              _id = createGuid();
-
              _shadowRoot.querySelector("#oView").id = _id + "_oView";
-
-             this._export_settings = {};
-         //    this._export_settings.date = "";
-
              this.addEventListener("click", event => {
-               
                  console.log('click');
+				 var event = new Event("onClick");
+                 this.dispatchEvent(event);
              });
-
-
-                 
          }
  
          //Fired when the widget is added to the html DOM of the page
          connectedCallback() {
-             try {
-                 if (window.commonApp) {
-                     let outlineContainer = commonApp.getShell().findElements(true, ele => ele.hasStyleClass && ele.hasStyleClass("sapAppBuildingOutline"))[0]; // sId: "__container0"
-
-                     if (outlineContainer && outlineContainer.getReactProps) {
-                         let parseReactState = state => {
-                             let components = {};
-
-                             let globalState = state.globalState;
-                             let instances = globalState.instances;
-                             let app = instances.app["[{\"app\":\"MAIN_APPLICATION\"}]"];
-                             let names = app.names;
-
-                             for (let key in names) {
-                                 let name = names[key];
-
-                                 let obj = JSON.parse(key).pop();
-                                 let type = Object.keys(obj)[0];
-                                 let id = obj[type];
-
-                                 components[id] = {
-                                     type: type,
-                                     name: name
-                                 };
-                             }
-
-                             for (let componentId in components) {
-                                 let component = components[componentId];
-                             }
-
-                             let metadata = JSON.stringify({
-                                 components: components,
-                                 vars: app.globalVars
-                             });
-
-                             if (metadata != this.metadata) {
-                                 this.metadata = metadata;
-
-                                 this.dispatchEvent(new CustomEvent("propertiesChanged", {
-                                     detail: {
-                                         properties: {
-                                             metadata: metadata
-                                         }
-                                     }
-                                 }));
-                             }
-                         };
-
-                         let subscribeReactStore = store => {
-                             this._subscription = store.subscribe({
-                                 effect: state => {
-                                     parseReactState(state);
-                                     return {
-                                         result: 1
-                                     };
-                                 }
-                             });
-                         };
-
-                         let props = outlineContainer.getReactProps();
-                         if (props) {
-                             subscribeReactStore(props.store);
-                         } else {
-                             let oldRenderReactComponent = outlineContainer.renderReactComponent;
-                             outlineContainer.renderReactComponent = e => {
-                                 let props = outlineContainer.getReactProps();
-                                 subscribeReactStore(props.store);
-
-                                 oldRenderReactComponent.call(outlineContainer, e);
-                             }
-                         }
-                     }
-                 }
-             } catch (e) { }
+			this._firstConnection = true;
+            loadthis(this);   
          }
  
           //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-         disconnectedCallback() {
-             if (this._subscription) {
-                 this._subscription();
-                 this._subscription = null;
-             }
-         
+         disconnectedCallback() {         
          }
  
           //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-         onCustomWidgetBeforeUpdate(oChangedProperties) {
-             if ("designMode" in oChangedProperties) {
-                 this._designMode = oChangedProperties["designMode"];
-             }
- 
+         onCustomWidgetBeforeUpdate(oChangedProperties) { 
          }
  
          //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
          onCustomWidgetAfterUpdate(oChangedProperties) {
-             loadthis(this);  
+			if (this._firstConnection){
+                loadthis(this); 
+            }
+              
          }
-
 
          _firePropertiesChanged() {
              this.tileId = "";
@@ -206,22 +117,12 @@
                         console.log("The GenericTile is pressed.");
 						this.settings = {};
                       //  this.settings.password = "";
-
                         that.dispatchEvent(new CustomEvent("onClick", {
                             detail: {
                                 settings: this.settings
                             }
                         }));
-                    },
-					header: function(oEvent) {
-						console.log("The Header Loading.");
-						this.settings = {};
-                        that.dispatchEvent(new CustomEvent("onLoad", {
-                            detail: {
-                                settings: this.settings
-                            }
-                        }));
-					}
+                    }
                 });
             });
 
@@ -230,7 +131,6 @@
                 viewContent: jQuery(_shadowRoot.getElementById(_id + "_oView")).html(),
             });
             oView.placeAt(content);
-
            
             if (that_._designMode) {
                 oView.byId("GrossMargin").setEnabled(false);
